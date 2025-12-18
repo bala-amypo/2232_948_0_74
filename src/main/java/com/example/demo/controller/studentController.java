394dfe0bf3e7824625b65a4beaeba36a557dc9f3
bala-amypo <bala@amypo.in>
2025-12-18 02:25:37 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -1,11 +1,45 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.studentEntity;
+import com.example.demo.service.studentService;
 
-import com.example.demo.entity.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class studentController {
 
     @Autowired
+    studentService service;
+
+    @GetMapping("/getAllStudent")
+    public List<studentEntity> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping("/add")
+    public studentEntity addStudent(
+            @Valid @RequestBody studentEntity student) {
+        return service.addStudent(student);
+    }
+
+    @GetMapping("/get/{id}")
+    public studentEntity getStudentById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateStudent(
+            @PathVariable Long id,
+            @RequestBody studentEntity student) {
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable long id) {
+        return service.deleteStudent(id);
+    }
 }
