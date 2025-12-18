8f43bc4e3f1d2da2e88580a34d59dea0cfd138bd
bala-amypo <bala@amypo.in>
2025-12-18 02:27:17 +0000

    User commit

--- src/main/java/com/example/demo/service/imple/studentServiceImpl.java
+++ src/main/java/com/example/demo/service/imple/studentServiceImpl.java
@@ -2,63 +2,55 @@ package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import com.example.demo.entity.*;
-import com.example.demo.service.repository.*;
-import com.example.demo.service.*;
+import com.example.demo.entity.studentEntity;
+import com.example.demo.repository.studentRepo;
+import com.example.demo.service.studentService;
+
 import java.util.*;
 
 @Service
 public class studentServiceImpl implements studentService {
-    
 
     @Autowired
     studentRepo repo;
-    
+
     @Override
     public List<studentEntity> getAll() {
         return repo.findAll();
     }
 
-//     @Override
-//     public studentEntity addStudent(studentEntity stu) {
-//    return repo.save(stu);
-//     }
+    @Override
+    public studentEntity addStudent(studentEntity student) {
+        return repo.save(student);
+    }
 
-//    @Override
-//    public studentEntity addStudent(studentEntity stu) {
-//    return repo.save(stu);
-//    }
+    @Override
+    public studentEntity getById(Long id) {
+        Optional<studentEntity> data = repo.findById(id);
+        if (data.isPresent()) {
+            return data.get();
+        } else {
+            return null;
+        }
+    }
 
-//    @Override
-//    public studentEntity getById(@PathVariable Long id){
-//     Optional<studentEntity> data = repo.findById(id);
-//        if(data.isPresent()){
-//          return data.get();
-//        }else{
-//             return null;
-//              }
-//             }
+    @Override
+    public String updateStudent(Long id, studentEntity newstu) {
+        if (repo.existsById(id)) {
+            newstu.setId(id);
+            repo.save(newstu);
+            return "Student updated";
+        }
+        return "Student not found";
+    }
 
-//    @Override
-//    public String updateStudent(Long id, studentEntity newstu) {
-//    if(repo.existsById(id)){
-//    newstu.setId(id);
-//          repo.save(newstu);
-//          return "Student updated";
-//    }
-  
-//    return "Student not found"
-// } 
-
-//    @Override
-//    public String deleteStudent(long id){
-//     if(repo.existsById(id)){
-//         repo.deleteById(id);
-//         return "Student deleted";
-//     }
-   
-//     return "Student not found";
-// }
-   }
+    @Override
+    public String deleteStudent(long id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return "Student deleted";
+        }
+        return "Student not found";
+    }
+}
